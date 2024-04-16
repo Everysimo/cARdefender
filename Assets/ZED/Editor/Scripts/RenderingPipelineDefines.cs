329e9439caa9d4927b0566d0a4f7519cabd0a9d1
Everysimo <simone.spera.13@gmail.com>
2024-04-16 23:10:20 +0100

    feat(Zed): zed skd

--- Assets/ZED/Editor/Scripts/RenderingPipelineDefines.cs
+++ Assets/ZED/Editor/Scripts/RenderingPipelineDefines.cs
@@ -1,125 +1,127 @@
 using System.Collections.Generic;
 using System.Linq;
 using UnityEditor;
-using UnityEngine;
 using UnityEngine.Rendering;
 
-[InitializeOnLoad]
-public class RenderingPipelineDefines
+namespace ZED.Editor.Scripts
 {
-    enum PipelineType
+    [InitializeOnLoad]
+    public class RenderingPipelineDefines
     {
-        Unsupported,
-        BuiltInPipeline,
-        UniversalPipeline,
-        HDPipeline
-    }
-
-    static RenderingPipelineDefines()
-    {
-        UpdateDefines();
-    }
-
-    /// <summary>
-    /// Update the unity pipeline defines for URP
-    /// </summary>
-    static void UpdateDefines()
-    {
-        var pipeline = GetPipeline();
-
-        if (pipeline == PipelineType.UniversalPipeline)
+        enum PipelineType
         {
-            AddDefine("ZED_URP");
+            Unsupported,
+            BuiltInPipeline,
+            UniversalPipeline,
+            HDPipeline
         }
-        else
+
+        static RenderingPipelineDefines()
         {
-            RemoveDefine("ZED_URP");
+            UpdateDefines();
         }
-        if (pipeline == PipelineType.HDPipeline)
+
+        /// <summary>
+        /// Update the unity pipeline defines for URP
+        /// </summary>
+        static void UpdateDefines()
         {
-            AddDefine("ZED_HDRP");
+            var pipeline = GetPipeline();
+
+            if (pipeline == PipelineType.UniversalPipeline)
+            {
+                AddDefine("ZED_URP");
+            }
+            else
+            {
+                RemoveDefine("ZED_URP");
+            }
+            if (pipeline == PipelineType.HDPipeline)
+            {
+                AddDefine("ZED_HDRP");
+            }
+            else
+            {
+                RemoveDefine("ZED_HDRP");
+            }
         }
-        else
-        {
-            RemoveDefine("ZED_HDRP");
-        }
-    }
 
 
-    /// <summary>
-    /// Returns the type of renderpipeline that is currently running
-    /// </summary>
-    /// <returns></returns>
-    static PipelineType GetPipeline()
-    {
+        /// <summary>
+        /// Returns the type of renderpipeline that is currently running
+        /// </summary>
+        /// <returns></returns>
+        static PipelineType GetPipeline()
+        {
 #if UNITY_2019_1_OR_NEWER
-        if (GraphicsSettings.renderPipelineAsset != null)
-        {
-            // SRP
-            var srpType = GraphicsSettings.renderPipelineAsset.GetType().ToString();
-            if (srpType.Contains("HDRenderPipelineAsset"))
+            if (GraphicsSettings.renderPipelineAsset != null)
             {
-                return PipelineType.HDPipeline;
+                // SRP
+                var srpType = GraphicsSettings.renderPipelineAsset.GetType().ToString();
+                if (srpType.Contains("HDRenderPipelineAsset"))
+                {
+                    return PipelineType.HDPipeline;
+                }
+                else if (srpType.Contains("UniversalRenderPipelineAsset"))
+                {
+                    return PipelineType.UniversalPipeline;
+                }
+                else return PipelineType.Unsupported;
             }
-            else if (srpType.Contains("UniversalRenderPipelineAsset"))
-            {
-                return PipelineType.UniversalPipeline;
-            }
-            else return PipelineType.Unsupported;
-        }
 #elif UNITY_2017_1_OR_NEWER
         if (GraphicsSettings.renderPipelineAsset != null) {
             // SRP not supported before 2019
             return PipelineType.Unsupported;
         }
 #endif
-        // no SRP
-        return PipelineType.BuiltInPipeline;
-    }
-
-    /// <summary>
-    /// Add a custom define
-    /// </summary>
-    /// <param name="define"></param>
-    /// <param name="buildTargetGroup"></param>
-    public static void AddDefine(string define)
-    {
-        var definesList = GetDefines();
-        if (!definesList.Contains(define))
-        {
-            definesList.Add(define);
-            SetDefines(definesList);
+            // no SRP
+            return PipelineType.BuiltInPipeline;
         }
-    }
 
-    /// <summary>
-    /// Remove a custom define
-    /// </summary>
-    /// <param name="_define"></param>
-    /// <param name="_buildTargetGroup"></param>
-    public static void RemoveDefine(string define)
-    {
-        var definesList = GetDefines();
-        if (definesList.Contains(define))
+        /// <summary>
+        /// Add a custom define
+        /// </summary>
+        /// <param name="define"></param>
+        /// <param name="buildTargetGroup"></param>
+        public static void AddDefine(string define)
         {
-            definesList.Remove(define);
-            SetDefines(definesList);
+            var definesList = GetDefines();
+            if (!definesList.Contains(define))
+            {
+                definesList.Add(define);
+                SetDefines(definesList);
+            }
         }
-    }
 
-    public static List<string> GetDefines()
-    {
-        var target = EditorUserBuildSettings.activeBuildTarget;
-        var buildTargetGroup = BuildPipeline.GetBuildTargetGroup(target);
-        var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
-        return defines.Split(';').ToList();
-    }
+        /// <summary>
+        /// Remove a custom define
+        /// </summary>
+        /// <param name="_define"></param>
+        /// <param name="_buildTargetGroup"></param>
+        public static void RemoveDefine(string define)
+        {
+            var definesList = GetDefines();
+            if (definesList.Contains(define))
+            {
+                definesList.Remove(define);
+                SetDefines(definesList);
+            }
+        }
 
-    public static void SetDefines(List<string> definesList)
-    {
-        var target = EditorUserBuildSettings.activeBuildTarget;
-        var buildTargetGroup = BuildPipeline.GetBuildTargetGroup(target);
-        var defines = string.Join(";", definesList.ToArray());
-        PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, defines);
+        public static List<string> GetDefines()
+        {
+            var target = EditorUserBuildSettings.activeBuildTarget;
+            var buildTargetGroup = BuildPipeline.GetBuildTargetGroup(target);
+            var defines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTargetGroup);
+            return defines.Split(';').ToList();
+        }
+
+        public static void SetDefines(List<string> definesList)
+        {
+            var target = EditorUserBuildSettings.activeBuildTarget;
+            var buildTargetGroup = BuildPipeline.GetBuildTargetGroup(target);
+            var defines = string.Join(";", definesList.ToArray());
+            PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTargetGroup, defines);
+        }
     }
 }
--- Assets/ZED/Editor/Scripts/UpgradePluginToSRP.cs
+++ Assets/ZED/Editor/Scripts/UpgradePluginToSRP.cs
@@ -3,51 +3,147 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.Rendering;
 
-public class EditPrefabAssetScope : IDisposable
+namespace ZED.Editor.Scripts
 {
-
-    public readonly string assetPath;
-    public readonly GameObject prefabRoot;
-
-    public EditPrefabAssetScope(string assetPath)
+    public class EditPrefabAssetScope : IDisposable
     {
-        this.assetPath = assetPath;
-        prefabRoot = PrefabUtility.LoadPrefabContents(assetPath);
-    }
 
-    public void Dispose()
-    {
-        PrefabUtility.SaveAsPrefabAsset(prefabRoot, assetPath);
-        PrefabUtility.UnloadPrefabContents(prefabRoot);
-    }
-}
+        public readonly string assetPath;
+        public readonly GameObject prefabRoot;
 
-public class UpgradePluginToSRP
-{
-
-    [MenuItem("ZED/Upgrade ZED Plugin to URP")]
-    static void UpgradeToURP()
-    {
-        var SRP = GraphicsSettings.renderPipelineAsset;
-
-        string zedRigMonoPath = "Assets/ZED/Prefabs/ZED_Rig_Mono.prefab";
-        string zedRigStereoPath = "Assets/ZED/Prefabs/ZED_Rig_Stereo.prefab";
-        string zedGreenScreenPath = "Assets/ZED/Examples/Green Screen/Prefabs/ZED_GreenScreen.prefab";
-        string zedPlanetariumPath = "Assets/ZED/Examples/Planetarium/Prefabs/Planetarium.prefab";
-
-
-        if (GraphicsSettings.renderPipelineAsset != null)
+        public EditPrefabAssetScope(string assetPath)
         {
-            if (SRP.GetType().ToString().Contains("UniversalRenderPipelineAsset"))
+            this.assetPath = assetPath;
+            prefabRoot = PrefabUtility.LoadPrefabContents(assetPath);
+        }
+
+        public void Dispose()
+        {
+            PrefabUtility.SaveAsPrefabAsset(prefabRoot, assetPath);
+            PrefabUtility.UnloadPrefabContents(prefabRoot);
+        }
+    }
+
+    public class UpgradePluginToSRP
+    {
+
+        [MenuItem("ZED/Upgrade ZED Plugin to URP")]
+        static void UpgradeToURP()
+        {
+            var SRP = GraphicsSettings.renderPipelineAsset;
+
+            string zedRigMonoPath = "Assets/ZED/Prefabs/ZED_Rig_Mono.prefab";
+            string zedRigStereoPath = "Assets/ZED/Prefabs/ZED_Rig_Stereo.prefab";
+            string zedGreenScreenPath = "Assets/ZED/Examples/Green Screen/Prefabs/ZED_GreenScreen.prefab";
+            string zedPlanetariumPath = "Assets/ZED/Examples/Planetarium/Prefabs/Planetarium.prefab";
+
+
+            if (GraphicsSettings.renderPipelineAsset != null)
+            {
+                if (SRP.GetType().ToString().Contains("UniversalRenderPipelineAsset"))
+                {
+
+                    RenderingPipelineDefines.AddDefine("ZED_URP");
+                    RenderingPipelineDefines.RemoveDefine("ZED_HDRP");
+
+                    Material Mat_Zed_Forward_Lighting = Resources.Load("Materials/Lighting/Mat_ZED_Forward_Lighting_URP") as Material;
+                    Material Mat_Zed_GreenScreen = Resources.Load("Materials/Mat_ZED_GreenScreen_URP") as Material;
+                    Material Mat_Zed_Sun = Resources.Load("Sun/Materials/Sun_URP") as Material;
+
+
+                    // Modify prefab contents and save it back to the Prefab Asset
+                    using (var editScope = new EditPrefabAssetScope(zedRigMonoPath))
+                    {
+                        Transform frame = editScope.prefabRoot.transform.Find("Camera_Left").Find("Frame");
+
+                        if (frame)
+                        {
+                            frame.GetComponent<MeshRenderer>().material = Mat_Zed_Forward_Lighting;
+                        }
+                        else
+                        {
+                            Debug.Log("Frame is not found");
+                        }
+                    }
+                    using (var editScope = new EditPrefabAssetScope(zedRigStereoPath))
+                    {
+                        Transform frame_left = editScope.prefabRoot.transform.Find("Camera_eyes").Find("Left_eye").Find("Frame");
+
+                        if (frame_left)
+                        {
+                            frame_left.GetComponent<MeshRenderer>().material = Mat_Zed_Forward_Lighting;
+                        }
+                        else
+                        {
+                            Debug.Log("Frame Left is not found");
+                        }
+
+                        Transform frame_right = editScope.prefabRoot.transform.Find("Camera_eyes").Find("Right_eye").Find("Frame");
+
+                        if (frame_right)
+                        {
+                            frame_right.GetComponent<MeshRenderer>().material = Mat_Zed_Forward_Lighting;
+                        }
+                        else
+                        {
+                            Debug.Log("Frame Right is not found");
+                        }
+                    }
+                    // Modify prefab contents and save it back to the Prefab Asset
+                    using (var editScope = new EditPrefabAssetScope(zedGreenScreenPath))
+                    {
+                        Transform frame = editScope.prefabRoot.transform.Find("Camera_Left").Find("Frame");
+
+                        if (frame)
+                        {
+                            frame.GetComponent<MeshRenderer>().material = Mat_Zed_GreenScreen;
+                        }
+                        else
+                        {
+                            Debug.Log("Frame is not found");
+                        }
+                    }
+                    using (var editScope = new EditPrefabAssetScope(zedPlanetariumPath))
+                    {
+                        Transform sun = editScope.prefabRoot.transform.Find("Sun").Find("sun");
+
+                        if (sun)
+                        {
+                            sun.GetComponent<MeshRenderer>().material = Mat_Zed_Sun;
+                        }
+                        else
+                        {
+                            Debug.Log("Planetarium is not found");
+                        }
+                    }
+                }
+                else
+                {
+                    Debug.LogWarning("Trying to convert to URP without using URP !!");
+                }
+            }
+
+        }
+
+        [MenuItem("ZED/Upgrade ZED Plugin to HDRP")]
+        static void UpgradeToHDRP()
+        {
+            var SRP = GraphicsSettings.renderPipelineAsset;
+
+            string zedRigMonoPath = "Assets/ZED/Prefabs/ZED_Rig_Mono.prefab";
+            string zedRigStereoPath = "Assets/ZED/Prefabs/ZED_Rig_Stereo.prefab";
+            string zedGreenScreenPath = "Assets/ZED/Examples/GreenScreen/Prefabs/ZED_GreenScreen.prefab";
+            string zedPlanetariumPath = "Assets/ZED/Examples/Planetarium/Prefabs/Planetarium.prefab";
+
+            if (SRP.GetType().ToString().Contains("HDRenderPipelineAsset"))
             {
 
-                RenderingPipelineDefines.AddDefine("ZED_URP");
-                RenderingPipelineDefines.RemoveDefine("ZED_HDRP");
+                RenderingPipelineDefines.AddDefine("ZED_HDRP");
+                RenderingPipelineDefines.RemoveDefine("ZED_URP");
 
-                Material Mat_Zed_Forward_Lighting = Resources.Load("Materials/Lighting/Mat_ZED_Forward_Lighting_URP") as Material;
-                Material Mat_Zed_GreenScreen = Resources.Load("Materials/Mat_ZED_GreenScreen_URP") as Material;
+                Material Mat_Zed_Forward_Lighting = Resources.Load("Materials/Lighting/Mat_ZED_HDRP_Lit") as Material;
+                Material Mat_Zed_GreenScreen = Resources.Load("Materials/Mat_ZED_Greenscreen_HDRP_Lit") as Material;
                 Material Mat_Zed_Sun = Resources.Load("Sun/Materials/Sun_URP") as Material;
-
 
                 // Modify prefab contents and save it back to the Prefab Asset
                 using (var editScope = new EditPrefabAssetScope(zedRigMonoPath))
@@ -63,6 +159,7 @@ public class UpgradePluginToSRP
                         Debug.Log("Frame is not found");
                     }
                 }
+
                 using (var editScope = new EditPrefabAssetScope(zedRigStereoPath))
                 {
                     Transform frame_left = editScope.prefabRoot.transform.Find("Camera_eyes").Find("Left_eye").Find("Frame");
@@ -117,106 +214,11 @@ public class UpgradePluginToSRP
             }
             else
             {
-                Debug.LogWarning("Trying to convert to URP without using URP !!");
+                Debug.LogWarning("Trying to convert to HDRP without using HDRP !!");
             }
         }
 
-    }
-
-    [MenuItem("ZED/Upgrade ZED Plugin to HDRP")]
-    static void UpgradeToHDRP()
-    {
-        var SRP = GraphicsSettings.renderPipelineAsset;
-
-        string zedRigMonoPath = "Assets/ZED/Prefabs/ZED_Rig_Mono.prefab";
-        string zedRigStereoPath = "Assets/ZED/Prefabs/ZED_Rig_Stereo.prefab";
-        string zedGreenScreenPath = "Assets/ZED/Examples/GreenScreen/Prefabs/ZED_GreenScreen.prefab";
-        string zedPlanetariumPath = "Assets/ZED/Examples/Planetarium/Prefabs/Planetarium.prefab";
-
-        if (SRP.GetType().ToString().Contains("HDRenderPipelineAsset"))
-        {
-
-            RenderingPipelineDefines.AddDefine("ZED_HDRP");
-            RenderingPipelineDefines.RemoveDefine("ZED_URP");
-
-            Material Mat_Zed_Forward_Lighting = Resources.Load("Materials/Lighting/Mat_ZED_HDRP_Lit") as Material;
-            Material Mat_Zed_GreenScreen = Resources.Load("Materials/Mat_ZED_Greenscreen_HDRP_Lit") as Material;
-            Material Mat_Zed_Sun = Resources.Load("Sun/Materials/Sun_URP") as Material;
-
-            // Modify prefab contents and save it back to the Prefab Asset
-            using (var editScope = new EditPrefabAssetScope(zedRigMonoPath))
-            {
-                Transform frame = editScope.prefabRoot.transform.Find("Camera_Left").Find("Frame");
-
-                if (frame)
-                {
-                    frame.GetComponent<MeshRenderer>().material = Mat_Zed_Forward_Lighting;
-                }
-                else
-                {
-                    Debug.Log("Frame is not found");
-                }
-            }
-
-            using (var editScope = new EditPrefabAssetScope(zedRigStereoPath))
-            {
-                Transform frame_left = editScope.prefabRoot.transform.Find("Camera_eyes").Find("Left_eye").Find("Frame");
-
-                if (frame_left)
-                {
-                    frame_left.GetComponent<MeshRenderer>().material = Mat_Zed_Forward_Lighting;
-                }
-                else
-                {
-                    Debug.Log("Frame Left is not found");
-                }
-
-                Transform frame_right = editScope.prefabRoot.transform.Find("Camera_eyes").Find("Right_eye").Find("Frame");
-
-                if (frame_right)
-                {
-                    frame_right.GetComponent<MeshRenderer>().material = Mat_Zed_Forward_Lighting;
-                }
-                else
-                {
-                    Debug.Log("Frame Right is not found");
-                }
-            }
-            // Modify prefab contents and save it back to the Prefab Asset
-            using (var editScope = new EditPrefabAssetScope(zedGreenScreenPath))
-            {
-                Transform frame = editScope.prefabRoot.transform.Find("Camera_Left").Find("Frame");
-
-                if (frame)
-                {
-                    frame.GetComponent<MeshRenderer>().material = Mat_Zed_GreenScreen;
-                }
-                else
-                {
-                    Debug.Log("Frame is not found");
-                }
-            }
-            using (var editScope = new EditPrefabAssetScope(zedPlanetariumPath))
-            {
-                Transform sun = editScope.prefabRoot.transform.Find("Sun").Find("sun");
-
-                if (sun)
-                {
-                    sun.GetComponent<MeshRenderer>().material = Mat_Zed_Sun;
-                }
-                else
-                {
-                    Debug.Log("Planetarium is not found");
-                }
-            }
-        }
-        else
-        {
-            Debug.LogWarning("Trying to convert to HDRP without using HDRP !!");
-        }
-    }
-
-    /*[MenuItem("ZED/Revert ZED Plugin to built-in RP")]
+        /*[MenuItem("ZED/Revert ZED Plugin to built-in RP")]
     static void RevertToBuiltInRP()
     {
         RenderingPipelineDefines.RemoveDefine("ZED_HDRP");
@@ -297,6 +299,5 @@ public class UpgradePluginToSRP
             }
         }
     }*/
+    }
 }
-
-
